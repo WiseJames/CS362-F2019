6f390b6f2681b27a0da592a84550491227d43d70
wiseja@oregonstate.edu <wiseja@oregonstate.edu>
2019-12-02 21:23:10 -0800

    recreate since it was overwritten by bug8

---
+++ projects/FinalProject-Bugs/dominion/unittestbug2.c
@@ -0,0 +1,36 @@
+#include "dominion.h"
+#include <stdio.h>
+#include "rngs.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+// assert functionality helped by: dillonbhuff.com/?p=439
+#define STR(x) #x
+#define MYASSERT(x) if (!(x)) {	printf("Assert failed: (%s), function %s, file %s, line %d.\n", STR(x), __PRETTY_FUNCTION__, __FILE__, __LINE__);}
+
+int main(int argc, char** argv) {
+
+	// set card array
+	int k[10] = { adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall };
+
+	// declare the game state
+	struct gameState G;
+
+	memset(&G, 0, sizeof(struct gameState)); 	// set the game state
+	initializeGame(2, k, 1, &G); 				// init the game
+
+	G.hand[0][1] = gold;				// place gold card at position 1
+
+	int choice1 = 1;
+	int choice2 = adventurer;			// place card choice as adventurer card
+	int choice3 = 0;
+	int handPos = 1;
+	int *bonus = 0;
+
+	int ret = cardEffect(mine, choice1, choice2, choice3, &G, handPos, bonus);
+
+	MYASSERT(ret != -1);
+
+	return 0;
+}
