6c482b2fbf443ad6a76f68cc88b4c444a3b49011
nseabourn <nseabourn001@gmail.com>
2019-12-03 19:19:32 -0800

    bug 5 done

--- projects/FinalProject-Bugfree/dominion/unittestbug7.c
+++ projects/FinalProject-Bugfree/dominion/unittestbug7.c
@@ -4,10 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <signal.h>
 
 // assert functionality helped by: dillonbhuff.com/?p=439
 #define STR(x) #x
 #define MYASSERT(x) if (!(x)) {	printf("Assert failed: (%s), function %s, file %s, line %d.\n", STR(x), __PRETTY_FUNCTION__, __FILE__, __LINE__);}
+
+void segv_handler(int sig)
+{
+	MYASSERT(false);
+	exit(0);
+}
 
 int main(int argc, char** argv) {
 
@@ -33,9 +40,14 @@ int main(int argc, char** argv) {
 	int actions = G.numActions + 2;
 	int handsize = G.handCount[G.whoseTurn] - 1;
 
+	//to catch seg fault
+	signal(SIGSEGV, segv_handler);
+
 	cardEffect(tribute, 0, 0, 0, &G, 0, 0);
 
 	MYASSERT(G.coins == coins && G.numActions == actions && G.handCount[G.whoseTurn] == handsize);
+
+	signal(SIGSEGV, SIG_DFL);
 
 	return 0;
 }
